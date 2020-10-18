bed671755e070bdcae32adbdb8a1a674d23f5828
Matthew Pagan <matthew.a.pagan@gmail.com>
2020-10-18 13:06:18 -0400

    Adding files

---
+++ configs.py
@@ -0,0 +1,37 @@
+dbname = 'sentiment.sqlite'
+projroot = '/Users/mttpgn/src/wsbdiscordsentiment'
+ss_location = '{}/screenshots'.format(projroot)
+db_location = '{}/{}'.format(projroot, dbname)
+exten = '.png'
+ss_top = 175
+ss_left = 820
+ss_width = 1985
+ss_height = 1645
+
+negative_wordlist = [
+  'drill', 
+  'puts', 
+  '0p', 
+  '1p', 
+  '2p', 
+  '3p', 
+  '4p', 
+  '5p', 
+  '6p', 
+  '7p', 
+  '8p', 
+  '9p', 
+  'fuck', 
+  'FUCK', 
+  'Fuck', 
+  'DRILL', 
+  'ROPE', 
+  'imagine', 
+  'Imagine', 
+  'drop', 
+  'shit',
+  'SHIT',
+  'not',
+  'NOT'
+]                                                                        
+
---
+++ discord_extractor.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python3
+
+from datetime import datetime
+import time
+import pyautogui
+import pytesseract as tess
+import sqlite3
+import os
+from PIL import Image
+from configs import *
+
+with open('{}/tickers.txt'.format(projroot)) as tickers:
+    rawtickers = tickers.read().split('\n')
+    tickerforms = [ \
+      [ \
+        ' {} '.format(t), \
+        ' {} '.format(t.lower()), \
+        ' ${} '.format(t), \
+        ' {} '.format(t.title()) 
+      ] for t in rawtickers
+                  ]
+    numTickerforms = len(tickerforms[0])
+
+def main():
+  while True:
+    newestfname = datetime.now().strftime(
+      '{}/%Y%m%d%H%m%S{}'.format(
+        ss_location, 
+        exten
+                                )
+                                         )
+    pyautogui.screenshot(
+      newestfname, 
+      region=(
+        ss_left, 
+        ss_top, 
+        ss_width, 
+        ss_height 
+             )
+                        )
+    img = Image.open(newestfname)
+    latestChatsPre = tess.image_to_string(img).split('\n')
+    latestChats = [t for t in latestChatsPre if '(' not in t and ')' not in t]
+    connection = None
+    while(connection == None):
+      try:
+        connection = connectToDatabase()
+      except e:
+        print("Failed to connect: {} ... retrying".format(e))
+        time.sleep(0.01)
+    lastDataBatch = []
+    thisDataBatch = []
+    for chatTxt in latestChats:
+      for tickerformat in tickerforms:
+        for i in range(numTickerforms):
+          if tickerformat[i] in chatTxt:
+            if chatTxt not in lastDataBatch and chatTxt not in thisDataBatch:
+              print(chatTxt)
+              insertChatData(chatTxt, connection, tickerformat[0].strip())
+              thisDataBatch.append(chatTxt) # To prevent duplicate data entries
+    connection.close()
+    lastDataBatch = thisDataBatch
+    time.sleep(5)
+    os.remove(newestfname)
+
+def connectToDatabase():
+  conn = None
+  try:
+    conn = sqlite3.connect(db_location)
+    print(sqlite3.version)
+  except Error as e:
+    print(e)
+  finally:
+    return conn
+
+def insertChatData(textData, cn, symbol):
+  cursor = cn.cursor()
+  query = """INSERT INTO messages 
+             (text, positive, timestamp, ticker_symbol) 
+             VALUES 
+             ("{}", "{}", {}, "{}")""".format(
+    textData, \
+    not any(negativeStr in textData for negativeStr in negative_wordlist), \
+    int(time.time()), \
+    symbol \
+                                       )
+  print(query)
+  count = cursor.execute(query)
+  cn.commit()
+  cursor.close()
+
+if __name__ == '__main__':
+  main()
