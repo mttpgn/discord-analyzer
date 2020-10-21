cfcf1595bd741fb4167f0cac8819d4b8d286e64f
Matthew Pagan <matthew.a.pagan@gmail.com>
2020-10-21 14:40:04 -0400

    Run during market hours

--- discord_extractor.py
+++ discord_extractor.py
@@ -15,7 +15,7 @@ with open('{}/tickers.txt'.format(projroot)) as tickers:
       [ \
         ' {} '.format(t), \
         ' {} '.format(t.lower()), \
-        ' ${} '.format(t), \
+        '${} '.format(t), \
         ' {} '.format(t.title()) 
       ] for t in rawtickers if t != ''
                   ]
@@ -23,50 +23,53 @@ with open('{}/tickers.txt'.format(projroot)) as tickers:
 
 def main():
   while True:
-    newestfname = datetime.now().strftime(
-      '{}/%Y%m%d%H%m%S{}'.format(
-        ss_location, 
-        exten
-                                )
-                                         )
-    pyautogui.screenshot(
-      newestfname, 
-      region=(
-        ss_left, 
-        ss_top, 
-        ss_width, 
-        ss_height 
-             )
-                        )
-    img = Image.open(newestfname)
-    latestChatsPre = tess.image_to_string(img).split('\n')
-    latestChats = [t for t in latestChatsPre if '(' not in t and ')' not in t]
-    latestChatsCleaned = ["".join( list( filter(lambda x : x in "QWERTYUIOPLKJHGFDSAZXCVBNM,.! qwertyuioplkjhgfdsazxcvbnm1234567890$%*&^", line) ) ) for line in latestChats]
-    # print('\nlatestChatsCleaned\n##################')
-    # print(latestChatsCleaned)
-    connection = None
-    while(connection == None):
-      try:
-        connection = connectToDatabase()
-      except e:
-        print("Failed to connect: {} ... retrying".format(e))
-        time.sleep(0.01)
-    lastDataBatch = []
-    thisDataBatch = []
-    for chatTxt in latestChatsCleaned:
-      for tickerformat in tickerforms:
-        for i in range(numTickerforms):
-          if tickerformat[i] in chatTxt:
-            if chatTxt not in lastDataBatch and chatTxt not in thisDataBatch:
-              print(chatTxt)
-              insertChatData(chatTxt, connection, tickerformat[0].strip())
-              thisDataBatch.append(chatTxt) # To prevent duplicate data entries
-    connection.close()
-    lastDataBatch = thisDataBatch
-    time.sleep(5)
-    os.remove(newestfname)
-    pyautogui.move(-30, 0)
-    pyautogui.move(30, 0)
+    if datetime.now().hour in list(range(9,16)):
+      newestfname = datetime.now().strftime(
+        '{}/%Y%m%d%H%m%S{}'.format(
+          ss_location, 
+          exten
+                                  )
+                                           )
+      pyautogui.screenshot(
+        newestfname, 
+        region=(
+          ss_left, 
+          ss_top, 
+          ss_width, 
+          ss_height 
+               )
+                          )
+      img = Image.open(newestfname)
+      latestChatsPre = tess.image_to_string(img).split('\n')
+      latestChats = [t for t in latestChatsPre if '(' not in t and ')' not in t]
+      latestChatsCleaned = ["".join( list( filter(lambda x : x in "QWERTYUIOPLKJHGFDSAZXCVBNM,.! qwertyuioplkjhgfdsazxcvbnm1234567890$%*&^", line) ) ) for line in latestChats]
+      # print('\nlatestChatsCleaned\n##################')
+      # print(latestChatsCleaned)
+      connection = None
+      while(connection == None):
+        try:
+          connection = connectToDatabase()
+        except e:
+          print("Failed to connect: {} ... retrying".format(e))
+          time.sleep(0.01)
+      lastDataBatch = []
+      thisDataBatch = []
+      for chatTxt in latestChatsCleaned:
+        for tickerformat in tickerforms:
+          for i in range(numTickerforms):
+            if tickerformat[i] in chatTxt:
+              if chatTxt not in lastDataBatch and chatTxt not in thisDataBatch:
+                print(chatTxt)
+                insertChatData(chatTxt, connection, tickerformat[0].strip())
+                thisDataBatch.append(chatTxt) # To prevent duplicate data entries
+      connection.close()
+      lastDataBatch = thisDataBatch
+      time.sleep(5)
+      os.remove(newestfname)
+      pyautogui.move(-30, 0)
+      pyautogui.move(30, 0)
+    else:
+      time.sleep(3600)
 
 def connectToDatabase():
   conn = None
